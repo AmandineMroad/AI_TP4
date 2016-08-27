996bee68f437fb4406ed2764ca8a74a8dbef13ae
amandine <roger.amandine@gmail.com>
2016-08-27 22:45:50 +0200

    Classe CustomPixel

---
+++ CustomPixel.cpp
@@ -0,0 +1,81 @@
+/* 
+ * Analyse d'images TP4
+ * IRM2016 ReVA
+ * File:   CustomPixel.cpp
+ * Author: Amandine ROGER
+ */
+
+#include "CustomPixel.h"
+
+CustomPixel::CustomPixel(int couleur, CustomPixel * voisinHaut, CustomPixel * voisinGauche){
+ this->valeur = couleur;
+ this->vHaut = voisinHaut;
+ this->vGauche = voisinGauche;
+ this->etiquette = -1;
+}
+
+char CustomPixel::GetValeur() const {
+    return valeur;
+}
+
+
+void CustomPixel::SetEtiquette(int etiquette) {
+    this->etiquette = etiquette;
+}
+
+int CustomPixel::GetEtiquette() const {
+    return etiquette;
+}
+
+ /**
+ * Convertit une image en un tableau de CustomPixel
+ * @param image l'image à convertir
+ * @return le tableau de CustomPixel correspondant à l'image
+ */
+CustomPixel** CustomPixel::imageToCustomPixelArray(IplImage * image){
+    int width = image->width;
+    int height = image->height;
+    
+    pixels = new CustomPixel[width][height];
+    
+    int i,j;
+    CustomPixel * haut,
+            * gauche;
+    
+    for (i=0; i<width; i++){
+        for (j=0; j<height; j++){
+            //Si pixel de début de ligne
+            if (i == 0) {
+                gauche = NULL;
+            } else {
+                gauche = &pixels[i-1][j];
+            }
+            
+            //si pixel de début de colonne
+            if (j == 0) {
+                haut = NULL;
+            } else {
+                haut = &pixels[i][j-1];
+            }
+            
+            //Création du pixel
+            pixels[i][j] = new CustomPixel(cvGet2D(image,i,j).val[0], haut, gauche);
+        }
+    }
+    
+    return pixels;
+}
+
+IplImage* CustomPixelArrayToImage(CustomPixel** pixels, int nbLignes, int nbColonnes){
+    IplImage* img = cvCreateImage(cvSize(nbLignes,nbColonnes),IPL_DEPTH_8U,1);
+    
+    int i,j,offset;
+    for (i=0; i<nbLignes ; i++){
+        offset = i*nbColonnes;
+        for(j=0; j<nbColonnes; j++){
+            img->imageData[j+offset] = pixels[i][j].GetValeur();
+        }
+    }
+    
+    return img;
+}
---
+++ CustomPixel.h
@@ -0,0 +1,37 @@
+/* 
+ * Analyse d'images TP4
+ * IRM2016 ReVA
+ * File:   CustomPixel.h
+ * Author: Amandine ROGER
+ */
+#ifndef CUSTOMPIXEL_H
+#define CUSTOMPIXEL_H
+
+#include <opencv2/core/types_c.h>
+
+#include <stddef.h>
+
+using namespace std;
+
+class CustomPixel {
+public:
+    CustomPixel(int valeur, CustomPixel * voisinHaut, CustomPixel * voisinGauche);
+
+    void SetEtiquette(int etiquette);
+    int GetEtiquette() const;
+    
+    //Mapping
+    static CustomPixel** imageToCustomPixelArray(IplImage * image);
+    static IplImage * CustomPixelArrayToImage(CustomPixel** pixels, int nbLignes, int nbColonnes);
+    char GetValeur() const;
+    
+private:
+    char valeur;
+    int etiquette;
+    CustomPixel * vHaut;
+    CustomPixel * vGauche;
+
+};
+
+#endif /* CUSTOMPIXEL_H */
+
